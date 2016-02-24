704c7f414b1eda2b2de53b14906e7c5a716d02d5
Martín Coll <t-martig@microsoft.com>
2016-02-23 18:05:56 -0800

    Initial

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var api = require('./api');
+
+app.use(express.static('public'));
+app.use('/api', api);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
